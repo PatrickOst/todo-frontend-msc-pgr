a17ceb7ce14c11cbf5248194a1cfc7bf327e2ecf
PatrickOst <patrick.gruenenfelder@ost.ch>
2021-10-27 18:34:20 +0200

    Input Field changed back to Textarea

--- src/components/pages/Overview.jsx
+++ src/components/pages/Overview.jsx
@@ -121,7 +121,7 @@ class NotesOverview extends Component {
 								checked = {u.erledigt}
 								onChange={this.updateErledigt(u.id)}
 							/>
-							<input className="note-description__container" type='text' placeholder='Note text' value={u.beschreibung} readOnly></input>
+							<textarea className="note-description__container" type='text' placeholder='Note text' value={u.beschreibung} readOnly></textarea>
 							<Link to={`/note/${u.id}`} className="modifynote__container">Bearbeiten</Link>
 						</div>
 					</Fragment>
